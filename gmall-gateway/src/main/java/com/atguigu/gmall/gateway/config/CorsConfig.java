7044bfbc3591c228d957d4c782d2e41a423a9d77
zm <767780721@qq.com>
2020-08-23 14:55:19 +0800

    谷粒商城项目第二天进程

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration  //配置类
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://manager.gmall.com");
+        corsConfiguration.addAllowedOrigin("http://www.gmall.com");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(configurationSource);
+    }
+}
